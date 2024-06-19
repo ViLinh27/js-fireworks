0cc3799d7a251c15b8902e8659c0b37915ac53e3
Vi-Linh Nguyen <vilinh27@gmail.com>
2024-06-19 11:18:24 -0500

    new class to set off fireworks and some other variables to help with that

--- canvas-fireworks.js
+++ canvas-fireworks.js
@@ -22,6 +22,26 @@
       anchorY: 0,
     };
 
+    const fireworks = [];
+    const flecks = [];
+    const flecks2 = [];
+    const flecks3 = [];
+    const numberOfFlecks = 25; //  bear in mind: performance gets worse with higher number of flecks
+
+    const random = (min, max) => Math.random() * (max - min) + min;
+
+    // calculate the distance between two points
+    // using Pythagorean theorem
+    // d = √x² + y², where x = x1 - x2, and y = y1 - y2
+    const getDistance = (x1, y1, x2, y2) => {
+        const xDistance = x1 - x2;
+        const yDistance = y1 - y2;
+    
+        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    };
+
+    let mouseClicked = false;
+
     const drawAnchor = () => {
         // get the position for the anchor on the canvas
         positions.anchorX = width / 2;
@@ -45,6 +65,10 @@
         positions.mouseX = e.pageX;
         positions.mouseY = e.pageY;
         });
+
+         // track mouse click events
+        canvas.addEventListener('mousedown', () => (mouseClicked = true));
+        canvas.addEventListener('mouseup', () => (mouseClicked = false));
     };
     
     const loop = () => {
@@ -56,5 +80,46 @@
         attachEventListeners();
         loop();
     });
+
+    // canvas-fireworks.js
+
+    class Firework {
+        constructor() {
+        const init = () => {
+            // Create the firework object
+            // current coordinates
+            this.x = positions.anchorX;
+            this.y = positions.anchorY;
+            
+            // target coordinates
+            this.target_x = positions.mouseX;
+            this.target_y = positions.mouseY;
+            
+            // distance from starting point to target
+            this.distanceToTarget = getDistance(
+                this.x,
+                this.y,
+                this.target_x,
+                this.target_y
+            );
+            this.distanceTraveled = 0;
+
+            this.coordinates = [];
+            this.angle = Math.atan2(
+                this.target_y - positions.anchorY,
+                this.target_x - positions.anchorX
+            );
+            this.speed = 15;
+            this.friction = 0.99;
+            this.hue = random(0, 360);
+
+            while (fireworkLength--) {
+                this.coordinates.push([this.x, this.y]);
+            }
+        }
+    
+        init();
+        }
+    }
   
   })();
